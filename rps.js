a99817dd36fc0fbf6f52164d3029536227df97b5
Hillary Adams <hradams4@asu.edu>
2024-05-31 17:04:09 -0400

    Add player choice function

--- rps.js
+++ rps.js
@@ -1,22 +1,46 @@
-let choice
+let computerChoice
+let playerChoice
+let promptPlayer
+
 function getComputerChoice() {
     let shoot = Math.floor(Math.random() * 3)
     console.log(shoot)
     switch (shoot) {
         case 0:
             choice = 'Rock'
-            console.log(choice)
+            console.log(computerChoice)
             break;
         case 1:
             choice = 'Paper'
-            console.log(choice)
+            console.log(computerChoice)
             break;
         case 2:
             choice = 'Scissors'
-            console.log(choice)
+            console.log(computerChoice)
             break;
         default:
             console.log('No choice was made by Computer')
     }
-    return choice;
+    return computerChoice;
+}
+
+function getHumanChoice(){
+    promptPlayer =  prompt("Your turn! Choose '1' for Rock, '2' for Paper, or '3' for Scissors")
+    switch (Number(promptPlayer)) {
+        case 1:
+            playerChoice = 'Rock'
+            console.log('Player chose ' + playerChoice)
+            break;
+        case 2:
+            playerChoice = 'Paper'
+            console.log('Player chose ' + playerChoice)
+            break;
+        case 3:
+            playerChoice = 'Scissors'
+            console.log('Player chose ' + playerChoice)
+            break;
+        default:
+            console.log('Invalid choice. You may only choose numbers 1-3.')
+    }
+    return playerChoice;
 }
